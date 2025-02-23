36eb1af03e7db8c24bab215707d9132d34e74245
Fadhil Yori <me@fadhilyori.my.id>
2025-02-23 23:12:43 +0700

    Add filename field and clearFileContent method to FileListener

--- internal/listener/file_listener.go
+++ internal/listener/file_listener.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"errors"
+	"os"
 	"sync/atomic"
 	"time"
 
@@ -18,6 +19,7 @@ import (
 )
 
 type FileListener struct {
+	filename     string
 	tail         *tail.Tail
 	linesPerSec  atomic.Int64
 	linesThisSec atomic.Int64
@@ -30,8 +32,23 @@ func NewFileListener(filename string) (*FileListener, error) {
 	}
 
 	return &FileListener{
-		tail: t,
+		filename: filename,
+		tail:     t,
 	}, nil
+}
+
+func (f *FileListener) clearFileContent() {
+	file, err := os.OpenFile(f.filename, os.O_WRONLY|os.O_TRUNC, 0644)
+	if err != nil {
+		log.WithFields(logger.Fields{
+			"error":   err,
+			"package": "file_listener",
+		}).Warnln("failed to clear file content")
+		return
+	}
+	defer file.Close()
+
+	log.Infof("Cleared file content: %s", f.filename)
 }
 
 func (f *FileListener) Start(ctx context.Context, q *queue.EventBatchQueue) error {
@@ -57,6 +74,7 @@ func (f *FileListener) Start(ctx context.Context, q *queue.EventBatchQueue) erro
 	defer func() {
 		ticker.Stop()
 		f.tail.Cleanup()
+		f.clearFileContent()
 		f.linesThisSec.Store(0)
 		f.linesPerSec.Store(0)
 
